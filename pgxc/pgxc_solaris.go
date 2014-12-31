53ea12dc662c56a775abe7c8bb1eb95538a99520
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-12-31 00:10:41 +0000

    Key authentication works on Smartos.

---
+++ pgxc/pgxc_solaris.go
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Copyright (c) 2014, K. S. Ernest "iFire" Lee
+
+package pgxc
+
+import (
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
+func authAgent() (auth ssh.AuthMethod) {
+        if(os.Getenv("SSH_AUTH_SOCK") == "") {
+		//return
+	}
+		
+	conn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer conn.Close()
+	ag := agent.NewClient(conn)
+	return ssh.PublicKeysCallback(ag.Signers)
+}
+
+// http://kukuruku.co/hub/golang/ssh-commands-execution-on-hundreds-of-servers-via-go
+
+func makeSigner(keyname string) (signer ssh.Signer, err error) {
+	fp, err := os.Open(keyname)
+	if err != nil {
+		return
+	}
+	defer fp.Close()
+
+	buf, _ := ioutil.ReadAll(fp)
+	signer, _ = ssh.ParsePrivateKey(buf)
+	return
+}
+
+func authKey() (auth ssh.AuthMethod) {
+        signers := []ssh.Signer{}
+	keys := []string{os.Getenv("HOME") + "/.ssh/id_rsa", os.Getenv("HOME") + "/.ssh/id_dsa"}
+
+	for _, keyname := range keys {
+		signer, err := makeSigner(keyname)
+		if err == nil {
+			signers = append(signers, signer)
+		}
+	}
+
+	return ssh.PublicKeys(signers...)
+}
+
+func Config(username string) *ssh.ClientConfig {
+	config := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{authKey()}, //authAgent()?
+	}
+	return config
+}
