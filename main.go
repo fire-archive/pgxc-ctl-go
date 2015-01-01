9482de1463aa8237a89962de58a5b62b46cb080b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2014-12-31 21:26:27 -0800

    Refactoring: move more things out of main.go.

--- main.go
+++ main.go
@@ -6,63 +6,15 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"github.com/fire/pgxc-ctl-go/pgxc"
-	"golang.org/x/crypto/ssh"
-	"log"
-	"net"
-	"os"
-	"runtime"
-	"strings"
 )
 
-// http://golang-basic.blogspot.ca/2014/06/step-by-step-guide-to-ssh-using-go.html
-// http://play.golang.org/p/kMhHvbl4SG
-type auth_info struct {
-	username string
-	server string
-}
-
-func execute(ai auth_info, cmds []string) {
-	var unixConn net.Conn
-	if runtime.GOOS != "windows" {
-		var err error
-		unixConn, err = net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-	}
-	client, err := ssh.Dial("tcp", ai.server, pgxc.Config(ai.username, unixConn))
-	if err != nil {
-		log.Fatalln("Failed to connect:", err)
-	}
-
-	session, err := client.NewSession()
-	if err != nil {
-		panic("Failed to create session: " + err.Error())
-	}
-	defer session.Close()
-	defer client.Close()
-	if runtime.GOOS != "windows" {
-		defer unixConn.Close()
-	}
-	
-	var b bytes.Buffer
-	session.Stdout = &b
-	if err := session.Run(strings.Join(cmds, ";")); err != nil {
-		fmt.Println(cmds)
-		panic("Failed to run: " + err.Error())
-	}
-	fmt.Println(b.String())
-}
-
 func main() {
-	var ai auth_info
-	ai.username = "admin"
-	ai.server   = "192.168.1.81:22"
+	var ai pgxc.Auth_info
+	ai.Username = "admin"
+	ai.Server = "192.168.1.81:22"
 	var cmds []string
 	cmds = append(cmds, "/usr/bin/env ls")
 	cmds = append(cmds, "/usr/bin/env ifconfig")
-	execute(ai, cmds)
+	pgxc.Execute(ai, cmds)
 }
---
+++ pgxc/pgxc.go
@@ -0,0 +1,52 @@
+package pgxc
+
+import (
+	"bytes"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"log"
+	"net"
+	"os"
+	"runtime"
+	"strings"
+)
+
+// http://golang-basic.blogspot.ca/2014/06/step-by-step-guide-to-ssh-using-go.html
+// http://play.golang.org/p/kMhHvbl4SG
+type Auth_info struct {
+	Username string
+	Server   string
+}
+
+func Execute(ai Auth_info, cmds []string) {
+	var unixConn net.Conn
+	if runtime.GOOS != "windows" {
+		var err error
+		unixConn, err = net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+	}
+	client, err := ssh.Dial("tcp", ai.Server, Config(ai.Username, unixConn))
+	if err != nil {
+		log.Fatalln("Failed to connect:", err)
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+	defer session.Close()
+	defer client.Close()
+	if runtime.GOOS != "windows" {
+		defer unixConn.Close()
+	}
+
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run(strings.Join(cmds, ";")); err != nil {
+		fmt.Println(cmds)
+		panic("Failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+}
